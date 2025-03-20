4159aa2ee1a6ae45126e4d1ed5b68458a891bc02
Huggorm91 <olaus@klaveness.dev>
2025-03-20 14:38:11 +0100

    Extracted Titlebar into a separate UserControl

---
+++ View/UserControls/TitleBar.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfTest.View.UserControls {
+	/// <summary>
+	/// Interaction logic for TitleBar.xaml
+	/// </summary>
+	public partial class TitleBar : UserControl {
+		private Window parent;
+
+		private bool forceQuit = false;
+		public bool ForceQuit { get; set; }
+
+
+		public TitleBar() {
+			InitializeComponent();
+
+			parent = Window.GetWindow(this);
+			Loaded += (sender, args) =>
+			{
+				parent = Window.GetWindow(this);
+			};			
+		}
+
+		private void TitleBar_Loaded(object sender, RoutedEventArgs e) {
+			throw new NotImplementedException();
+		}
+
+		private void btn_Close_Click(object sender, RoutedEventArgs e) {
+			if (forceQuit) {
+				Application.Current.Shutdown();
+			}
+			else {
+				parent.Close();
+			}
+		}
+
+		private void btn_Maximize_Click(object sender, RoutedEventArgs e) {
+			if (parent.WindowState == WindowState.Maximized) {
+				parent.WindowState = WindowState.Normal;
+				btn_Maximize.Content = "O";
+			}
+			else {
+				parent.WindowState = WindowState.Maximized;
+				btn_Maximize.Content = "o";
+			}
+		}
+
+		private void btn_Minimize_Click(object sender, RoutedEventArgs e) {
+			if (parent != null) {
+				parent.WindowState = WindowState.Minimized;
+			}
+		}
+	}
+}
