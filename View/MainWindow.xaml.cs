fe88b8ed82022996260a41b1afebc7e7cc7132d5
Huggorm91 <olaus@klaveness.dev>
2025-03-20 14:38:11 +0100

    Extracted Titlebar into a separate UserControl

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -5,22 +5,5 @@ namespace WpfTest {
 		public MainWindow() {
 			InitializeComponent();
 		}
-
-		private void btn_Close_Click(object sender, RoutedEventArgs e) {
-			Application.Current.Shutdown();
-        }
-
-		private void btn_Maximize_Click(object sender, RoutedEventArgs e) {
-			if (WindowState == WindowState.Maximized) {
-				WindowState = WindowState.Normal;
-			}
-			else {
-				WindowState = WindowState.Maximized;
-			}
-		}
-
-		private void btn_Minimize_Click(object sender, RoutedEventArgs e) {
-			WindowState = WindowState.Minimized;
-		}
 	}
 }
